883768d1e5120640355e6eacb821366e89fd306d
Lucas-wcs <112962818+Lucas-wcs@users.noreply.github.com>
2022-11-09 11:44:39 +0100

    Add files via upload

---
+++ backend/src/quizRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const quiz = require('./quiz');
+
+
+router.get("/", (req, res) => {
+    res.status(200).send(quiz);
+});
+
+router.get("/:id", (req, res) => {
+    const question = quiz.filter((quest) => {
+        return quest.id === req.params.id;
+    })
+    res.status(200).send(question);
+})
+
+module.exports = router;
