aa4cbecaac823cb71845961d01dc994e3304f3e8
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-10 14:50:13 +0100

    Lukasz_footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,24 +1,42 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookSquare,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import "../css/Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="info-container">
-        <ul className="list1-container">
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-        </ul>
-        <ul className="list2-container">
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-        </ul>
-        <ul className="list3-container">
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-          <li>Lorem ipsum</li>
-        </ul>
+      <div className="footer-content">
+        <div className="social-container">
+          <Link to="https://facebook.com/">
+            <FontAwesomeIcon className="fb icon" icon={faFacebookSquare} />
+          </Link>
+          <FontAwesomeIcon className="twitter icon" icon={faTwitter} />
+          <FontAwesomeIcon className="yt icon" icon={faYoutube} />
+          <FontAwesomeIcon className="email icon" icon={faEnvelope} />
+        </div>
+        <div className="info-container">
+          <ul className="list1-container">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+          <ul className="list2-container">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+          <ul className="list3-container">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
