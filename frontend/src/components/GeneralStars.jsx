540d4865e92b429b9bf120165f41e06a9353fec9
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2022-11-12 11:51:16 +0100

    Commit required before pull

--- frontend/src/components/GeneralStars.jsx
+++ frontend/src/components/GeneralStars.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-
-
-function GeneralStars() {
-  const [stars, setStars] = useState([]);
-
 import useWindowSize from "../hooks/useWindowSize";
 
 function GeneralStars() {
   const [stars, setStars] = useState([]);
   const [width, height] = useWindowSize();
-
 
   useEffect(() => {
     const tabTmp = [];
