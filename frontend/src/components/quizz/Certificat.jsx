ab93c0b45f2759b98a1a193922e6c7c11e1617c5
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-16 17:11:27 +0100

    Lukasz_errors fixed. Quizz fixed

---
+++ frontend/src/components/quizz/Certificat.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../css/quizz/Certificat.css";
+
+function Certificat() {
+  return (
+    <div className="certificat">
+      <h1 className="title-certificat">Certificat</h1>
+      <p className="p-certificat">
+        Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum lorem ipsum lorem{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Certificat;
