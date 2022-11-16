70d7c0f149ee66bd02f2732e959926c77df72b41
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-16 10:38:21 +0100

    Lukasz

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const planetRoutes = require('./planetRoutes');
-const quizRoutes = require('./quizRoutes');
+const planetRoutes = require("./planetRoutes");
+const quizRoutes = require("./quizRoutes");
 
 const app = express();
 
