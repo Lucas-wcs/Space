7a7ca5c6eae141446558960e5a24d80b9e654836
Nathalie  Dune <nathydune@gmail.com>
2022-11-18 16:59:17 +0100

    init nathalie

--- backend/src/planetRoutes.js
+++ backend/src/planetRoutes.js
@@ -1,17 +1,15 @@
-const router = require('express').Router();
-const planets = require('./planets');
-
-
+const router = require("express").Router();
+const planets = require("./planets");
 
 router.get("/", (req, res) => {
-    res.status(200).send(planets);
+  res.status(200).send(planets);
 });
 
 router.get("/:id", (req, res) => {
-    const planet = planets.filter((planet) => {
-        return planet.id === req.params.id;
-    })
-    res.status(200).send(planet);
-})
+  const planet = planets.filter((plan) => {
+    return plan.id === req.params.id;
+  });
+  res.status(200).send(planet);
+});
 
 module.exports = router;
